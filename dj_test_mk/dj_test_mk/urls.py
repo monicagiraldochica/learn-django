bb2ca3f2dcd748cdd248dc69fe7719d902f71029
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-09-17 14:44:18 -0500

    add basic hellow word url

--- dj_test_mk/dj_test_mk/urls.py
+++ dj_test_mk/dj_test_mk/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def hello(request):
+    return HttpResponse("<h1>Hello</h1><p>world</p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello)
 ]
